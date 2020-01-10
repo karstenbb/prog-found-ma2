b0bcac8d4c29e6e1af83a966a8c22ea2e8a68c4f
Karsten Bjelde <karsten.bjelde@iMac-6.local>
2020-01-10 12:40:25 +0100

    first commit

---
+++ js/master.js
@@ -0,0 +1,49 @@
+// question 1
+// your answer for question 1 goes here
+
+var petArray = ["cat", "cow", "dog"];
+
+// question
+// your answer for question 2 goes here
+console.log(petArray[1]);
+
+// question 3
+// your answer for question 3 goes here
+var petArray = ["cat", "cow", "dog"];
+petArray.push ("sheep");
+
+// question 4
+// your answer for question 4 goes here
+console.log(petArray.length);
+
+// question 5
+// your answer for question 5 goes here
+var catObject = {name: "julz",
+ colour: "black",
+ age: 100};
+
+ // question 6
+ // your answer for question 6 goes here
+ console.log(catObject.age);
+
+ // question 7
+ // your answer for question 7 goes here
+ var catArray = [catObject.name, catObject.colour, catObject.age];
+
+ // question 8
+ // your answer for question 8 goes here
+ for(var index = 0; index < catArray.length; index++){
+ console.log(catArray [index]);
+}
+
+// question 9
+// your answer for question 9 goes here
+
+function logToConsole(name) {
+  console.log("donkey");
+}
+
+// question 10
+// your answer for question 10 goes here
+
+logToConsole();
